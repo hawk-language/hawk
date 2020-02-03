a6ff12859c262387212c5f5246932e93aa6c8f20
mp1310 <pfeifenberger.markus2003@gmail.com>
2020-02-03 16:37:40 +0100

    Fixed Segfault when No file is given

--- src/main.c
+++ src/main.c
@@ -3,10 +3,16 @@
 //
 #include <hawk/lexer.h>
 #include <hawk/parser.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 
     //setbuf(stdout, NULL);
+    if(argc == 1){
+        printf("Welcome to the Hawk Programming Language!\n");
+        printf("Check it out on Github: https://github.com/hawk-language/hawk\n");
+        exit(0);
+    }
     Lexer lexer = new_Lexer(argv[1]);
     lexer.lex(&lexer);
 
