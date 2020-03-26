bd2356e50088063eaf90bb1e4552d7c97eba964d
Lockna <locknaraffi@gmail.com>
2020-03-26 19:42:12 +0100

    Defined enums AST_Node_Type and AST_BinExpr_Operator Defined structs AST_Number, AST_Variable, AST_BinExpr, AST_Call, AST_Prototype, AST_Prototype, AST_Function, AST_IF, AST_Node, AST

--- src/include/hawk/parser.h
+++ src/include/hawk/parser.h
@@ -5,5 +5,14 @@
 #ifndef HAWK_PARSER_H
 #define HAWK_PARSER_H
 
+#include <hawk/lexer.h>
+
+struct Parser {
+
+    struct token_list *TokenList;
+    struct AST *ast;
+    struct token_node *currentToken;
+
+};
 
 #endif //HAWK_PARSER_H
