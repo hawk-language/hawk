b88121c4c4a8150b0b0cc37fa4586bfeb5df0074
mp1310 <pfeifenberger.markus2003@gmail.com>
2020-04-13 14:17:56 +0200

    Added error handling when to many commandline arguments

--- src/main.c
+++ src/main.c
@@ -9,9 +9,13 @@ int main(int argc, char *argv[])
 {
 
         if (argc == 1) {
+                printf("Usage: hawk [file]\n");
                 printf("Welcome to the Hawk Programming Language!\n");
                 printf("Check it out on Github: https://github.com/hawk-language"
                        "/hawk\n");
+                exit(0);
+        }else if(argc > 2){
+                printf("Usage: hawk [file]\n");
                 exit(0);
         }
 
